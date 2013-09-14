4d52b1aee656a4ed58bce5610cd76c78d29da037
glukki <glukki.spb.ru@gmail.com>
2013-09-14 15:17:40 +0400

    передача параметров конфига rabbit_hint, rabbit_hint_score; опечатки с пропущенным *.config.* секции конфига rabbit_hint, rabbit_hint_score

--- conf/main.js
+++ conf/main.js
@@ -5,16 +5,19 @@ module.exports = {
 	flexo: undefined,
 	view: undefined,
 	controller: undefined,
-	
+
 	flexo_path: __dirname + '/../scheme/flexo',
 	link_path: __dirname + '/../scheme/link',
 	view_path: __dirname + '/../scheme/view',
 	template_path: __dirname + '/../view/template',
 	template_timeout: 100,
 	controller_role_to_company_view: {},
-	
+
 	redis: {
 		host: '127.0.0.1',
 		port: 6379
-	}
+	},
+
+	rabbit_hint: {},
+	rabbit_hint_score: {}
 };
